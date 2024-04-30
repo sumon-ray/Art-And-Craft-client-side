60c3e3cc61a52cdca49ea34011896f4bf5e71d8a
sumon <hydravulgaris60@gmail.com>
2024-04-30 23:58:09 +0600

    login, registration has setup

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -73,7 +73,7 @@ const Header = () => {
               </svg>
             </div>
             <ul
-            
+
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52"
             >
@@ -85,6 +85,7 @@ const Header = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="  px-1"> {navbar} </ul>
         </div>
+        
         <div className="navbar-end list-none flex relative">
           <div className="mr-2">
             <label className="swap swap-rotate">
